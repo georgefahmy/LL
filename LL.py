77aa89162f18d0b2f3fe72841cd92e6c99aac52c
George Fahmy <geofahm@gmail.com>
2023-06-29 16:46:14 -0700

    refactored whole thing

--- LL.py
+++ LL.py
@@ -3,76 +3,100 @@ import re
 import PySimpleGUI as sg
 
 from bs4 import BeautifulSoup as bs, SoupStrainer as ss
-from cryptography.fernet import Fernet
 from pprint import pprint
 
 
 BASE_URL = "https://www.learnedleague.com"
 
-key = Fernet.generate_key()
-fernet = Fernet(key)
+
+latest_season = bs(requests.get("https://www.learnedleague.com/allrundles.php").content, "html.parser", parse_only=ss("h1")).text.split(":")[0].split("LL")[-1]
+
+available_seasons = [str(season) for season in list(range(60, int(latest_season)+1, 1))]
 
 
-def encode(answer):
-    return fernet.encrypt(answer.encode())
+def get_categories(season_number):
+    categories_dict = {}
+    url = BASE_URL + "/match.php?" + str(season_number)
+    for i in range(1,26):
+        question_url = url + "&" + str(i)
+        questions = bs(requests.get(question_url).content, "html.parser").find_all("div", {"class": "ind-Q20 dont-break-out"})
+        categories = [
+            link.text.strip().split("-")[0].split(".")[-1].strip()
+            for link in questions
+        ]
+        for j, category in enumerate(categories):
+            categories_dict["D"+str(i)+"Q"+str(j+1)] = category
+    return categories_dict
 
+def get_all_questions(season_number):
+    all_questions_dict = {}
+    categories_dict = get_categories(season_number)
+    url = BASE_URL + "/match.php?" + str(season_number)
+    for i in range(1,26):
+        question_url = url + "&" + str(i)
+        page = bs(requests.get(question_url).content, "html.parser")
+        percentages = [
+            cell.text for cell in page.find_all("tr")[-2].find_all("td",{"class":"ind-Q3"})
+        ][2:-1]
+        questions = [
+            "-".join(link.text.strip().split("-")[1:]).strip()
+            for link in page.find_all("div", {"class": "ind-Q20 dont-break-out"})
+        ]
+        answers = [
+            link.text.strip()
+            for link in page.find_all("div", {"class": "a-red"})
+        ]
+        for j, question in enumerate(questions):
+            question_num_code = "D"+str(i)+"Q"+str(j+1)
+            all_questions_dict[question_num_code] = {
+                "_question": question,
+                "answer": answers[j],
+                "category": categories_dict[question_num_code],
+                "percent": percentages[j],
+                "question_num":question_num_code
+            }
 
-def decode(coded_answer):
-    return fernet.decrypt(coded_answer).decode()
+    return all_questions_dict
 
-
-def get_questions(season_number, min_threshold, max_threshold):
-    url = BASE_URL + "/allrundles.php?" + season_number
-
+def get_questions(all_questions_dict, min_threshold, max_threshold):
     min_threshold = int(min_threshold)
     max_threshold = int(max_threshold)
 
     if max_threshold <= min_threshold:
         max_threshold = min_threshold + 5
 
-    results = [
-        link
-        for link in bs(requests.get(url).content, "html.parser", parse_only=ss("a"))
-        if str(link.decode_contents()).isnumeric()
-        and int(str(link.decode_contents())) < max_threshold
-        and int(str(link.decode_contents())) > min_threshold
+    filtered_question_ids = [
+        question_ids
+        for question_ids, question in all_questions_dict.items()
+        if int(question["percent"]) >= min_threshold
+        and int(question["percent"]) < max_threshold
     ]
 
-    question_dict = {}
+    filtered_questions_dict = {
+        i+1: val
+        for i, val in enumerate(
+            [all_questions_dict[key] for key in filtered_question_ids]
+        )
+    }
 
-    for i, link in enumerate(results):
-        url = BASE_URL + link["href"]
-        soup = bs(requests.get(url).content, "html.parser")
-
-        question_num = "Day " + str(url.split("&")[1]) + " Q" + str(url.split("&")[2])
-
-        question_text = soup.find_all("div", {"class": "indivqQuestion"})[0].text.strip()
-        answer_text = soup.find_all("div", {"id": "xyz"})[0].span.text.strip()
-
-        question_dict[i + 1] = {
-            "_question": question_text,
-            "answer": encode(answer_text),
-            "percent": str(link.decode_contents()) + "%",
-            "question_num": question_num,
-        }
-
-    return question_dict
+    return filtered_questions_dict
 
 
 layout = [
     [
-        sg.Frame(
+        sg.Column([[sg.Frame(
             "Input",
+            element_justification="l",
             layout=[
                 [
-                    sg.Text("Season: ", font=("Arial", 16)),
+                    sg.Text("Season (min 60): ", font=("Arial", 16)),
                     sg.Text(expand_x=True),
-                    sg.Input(
-                        default_text="97",
+                    sg.Combo(
+                        available_seasons,
+                        default_value="97",
                         key="season",
                         font=("Arial", 16),
-                        size=(3, 1),
-                        justification="right",
+                        readonly=True,
                     ),
                 ],
                 [
@@ -99,22 +123,36 @@ layout = [
                 ],
                 [
                     sg.Text(expand_x=True),
-                    sg.Button("Retrieve Questions", key="retrieve"),
+                    sg.Button("Retrieve Season", key="retrieve"),
+                    sg.Button("Filter", key="filter", bind_return_key=True),
                     sg.Cancel(),
                 ],
             ],
-        ),
-        sg.Frame(
-            "Information",
+        ),]]),
+        sg.Column(
+        vertical_alignment="top",
+        layout =
+        [[sg.Frame(
+            "Season",
             vertical_alignment="top",
-            visible=False,
-            key="info_box",
+            visible=True,
+            expand_x = True,
+            key="season_info_box",
             layout=[
                 [
                     sg.Text("Season: ", font=("Arial", 16)),
                     sg.Text(expand_x=True),
                     sg.Text("", key="season_title", font=("Arial", 16)),
-                ],
+                ]
+            ]
+        )],
+        [sg.Frame(
+            "Information",
+            vertical_alignment="top",
+            visible=True,
+            expand_x = True,
+            key="info_box",
+            layout=[
                 [
                     sg.Text("Total Number of Questions: ", font=("Arial", 16)),
                     sg.Text(expand_x=True),
@@ -126,12 +164,17 @@ layout = [
                     sg.Text("", key="question_number", font=("Arial", 16)),
                 ],
                 [
+                    sg.Text("Category: ", font=("Arial", 16)),
+                    sg.Text(expand_x=True),
+                    sg.Text("", key="question_category", font=("Arial", 16)),
+                ],
+                [
                     sg.Text("Correct %: ", font=("Arial", 16)),
                     sg.Text(expand_x=True),
                     sg.Text("", key="%_correct", font=("Arial", 16)),
                 ],
             ],
-        ),
+        )]])
     ],
     [
         sg.Frame(
@@ -155,7 +198,14 @@ layout = [
                         "Answer",
                         expand_x=True,
                         layout=[
-                            [sg.Text(key="answer", font=("Arial", 16), size=(10, 1), expand_x=True)]
+                            [
+                                sg.Text(
+                                    key="answer",
+                                    font=("Arial", 16),
+                                    size=(10, 1),
+                                    expand_x=True
+                                )
+                            ]
                         ],
                     )
                 ],
@@ -209,15 +259,29 @@ while True:
         i = 1
 
     if event == "retrieve":
+        if values["season"] == window["season_title"].get():
+            print("season already loaded")
+            continue
+        all_questions_dict = get_all_questions(values["season"])
+        window["season_title"].update(value=values["season"])
+
+    if event == "filter":
         if int(values["min_%"]) > int(values["max_%"]):
-            values["max_%"] = str(int(values["min_%"]) + 5)
+            values["max_%"] = str(int(values["min_%"]) + 1)
             window["max_%"].update(value=values["max_%"])
 
-        if int(values["season"]) < 60:
-            values["season"] = "60"
-            window["season"].update(value="60")
+        if int(values["min_%"]) < 0:
+            values["min_%"] = str(0)
+            window["min_%"].update(value=values["max_%"])
 
-        questions = get_questions(values["season"], values["min_%"], values["max_%"])
+        if int(values["max_%"]) > 100 or int(values["min_%"]) > 100:
+            values["min_%"] = str(100)
+            values["max_%"] = str(100)
+            window["min_%"].update(value=values["min_%"])
+            window["max_%"].update(value=values["max_%"])
+
+
+        questions = get_questions(all_questions_dict, values["min_%"], values["max_%"])
 
         if not questions:
             window["question"].update(value="No Questions Available")
@@ -233,8 +297,9 @@ while True:
         window["info_box"].update(visible=True)
         window["season_title"].update(value=values["season"])
         window["num_questions"].update(value=len(list(questions.keys())))
-        window["%_correct"].update(value=question_object["percent"])
+        window["%_correct"].update(value=str(question_object["percent"]) + "%")
         window["question_number"].update(value=question_object["question_num"])
+        window["question_category"].update(value=question_object["category"])
         window["answer"].update(value="******")
         window["show/hide"].update(text="Show Answer")
         window["next"].update(disabled=False)
@@ -243,7 +308,7 @@ while True:
         if window["show/hide"].get_text() == "Show Answer":
             try:
                 window["show/hide"].update(text="Hide Answer")
-                window["answer"].update(value=decode(answer), font=("Arial", 16))
+                window["answer"].update(value=answer, font=("Arial", 16))
             except:
                 continue
 
@@ -268,7 +333,7 @@ while True:
         question = question_object.get("_question")
         answer = question_object.get("answer")
         window["question"].update(value=question)
-        window["%_correct"].update(value=question_object["percent"])
+        window["%_correct"].update(value=str(question_object["percent"]) + "%")
         window["question_number"].update(value=question_object["question_num"])
         if i == len(questions.keys()):
             window["next"].update(disabled=True)
@@ -291,8 +356,10 @@ while True:
         answer = question_object.get("answer")
         window["question"].update(value=question)
         window["dropdown"].update(value=i)
-        window["%_correct"].update(value=question_object["percent"])
+        window["%_correct"].update(value=str(question_object["percent"]) + "%")
         window["question_number"].update(value=question_object["question_num"])
+        window["question_category"].update(value=question_object["category"])
+
         if i == len(questions.keys()):
             window["next"].update(disabled=True)
         else:
@@ -314,8 +381,9 @@ while True:
         answer = question_object.get("answer")
         window["question"].update(value=question)
         window["dropdown"].update(value=i)
-        window["%_correct"].update(value=question_object["percent"])
+        window["%_correct"].update(value=str(question_object["percent"]) + "%")
         window["question_number"].update(value=question_object["question_num"])
+        window["question_category"].update(value=question_object["category"])
 
         if i == len(questions.keys()):
             window["next"].update(disabled=True)
